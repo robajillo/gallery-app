42ab7a30d90157f4dc5023e3c513af76574f5e11
robajillo <rjillo@yahoo.com>
2020-10-10 21:06:41 +0300

    updated class category

--- pictures/models.py
+++ pictures/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
