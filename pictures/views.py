fc862e926e8e2c7c5bbca5f8b173d396c50ffecb
robajillo <rjillo@yahoo.com>
2020-10-10 13:33:04 +0300

    created views for main index

--- pictures/views.py
+++ pictures/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 
-# Create your views here.
+def main(request):
+    return render(request,'index.html')
