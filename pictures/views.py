968e20c609117c0cc90cf0788d425989339215cd
robajillo <rjillo@yahoo.com>
2020-10-10 21:55:45 +0300

    imported models

--- pictures/views.py
+++ pictures/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
+from .models import Category, Location, Image
 
 def main(request):
     return render(request,'index.html')
