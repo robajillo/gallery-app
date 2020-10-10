9fc6ebd088ddff234aa61ec405a759b6025f93a3
robajillo <rjillo@yahoo.com>
2020-10-10 21:53:02 +0300

    created the search_results view function

--- pictures/views.py
+++ pictures/views.py
@@ -3,3 +3,17 @@ from django.http  import HttpResponse,Http404
 
 def main(request):
     return render(request,'index.html')
+
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_category = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"image": searched_category})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
+    
