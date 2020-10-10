b7e976e8f51619bc599d9aab33754852f82069f7
robajillo <rjillo@yahoo.com>
2020-10-10 21:15:06 +0300

    created class Image

--- pictures/models.py
+++ pictures/models.py
@@ -24,3 +24,18 @@ class Location(models.Model):
     def delete_location(self):
         self.delete()
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    # category = models.CharField(max_length=40)
+    # location = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
