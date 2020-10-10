05cd08eb1c592412a4da4a8ddbf8bbd62f1f5905
robajillo <rjillo@yahoo.com>
2020-10-10 21:08:33 +0300

    created class location

--- pictures/models.py
+++ pictures/models.py
@@ -12,3 +12,15 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
