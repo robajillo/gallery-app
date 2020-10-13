504b6ff572b395cb44bfaa0385613b557cb3be10
robajillo <rjillo@yahoo.com>
2020-10-13 13:27:01 +0300

    modified settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pictures.apps.PicturesConfig',
+    'pictures',
 ]
 
 MIDDLEWARE = [
